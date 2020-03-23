619aeecd42f64b1d0ac52ceca7d70c0e714820c1
tutkuince <tutku.ince@outlook.com>
2020-03-23 22:55:58 +0300

    Test Instance Lifecycle with @BeforeAll, @BeforeEach, @AfterEach and @AfterAll

--- src/test/java/com/muditasoft/junit5/_01helloworldtest/GreetingTest.java
+++ src/test/java/com/muditasoft/junit5/_01helloworldtest/GreetingTest.java
@@ -1,7 +1,6 @@
 package com.muditasoft.junit5._01helloworldtest;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,10 +8,20 @@ class GreetingTest {
 
     private Greeting greeting;
 
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Before - I am only called Once!!!");
+    }
+
     @BeforeEach
     void setUp() {
         System.out.println("In @BeforeEach. . .");
         greeting = new Greeting();
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("In @AfterEach. . .");
     }
 
     @Test
@@ -23,5 +32,10 @@ class GreetingTest {
     @Test
     void testHelloWorld() {
         System.out.println(greeting.helloWorld("Tutku"));
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("After - I am only called Once!!!");
     }
 }
