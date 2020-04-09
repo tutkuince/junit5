d08f11bc6ce8454a07f2329cb5c7b9900be6a46e
tutkuince <tutku.ince@outlook.com>
2020-04-09 16:04:30 +0300

    if check for both scenario

--- src/main/java/com/muditasoft/unittest/FizzBuzz.java
+++ src/main/java/com/muditasoft/unittest/FizzBuzz.java
@@ -3,6 +3,9 @@ package com.muditasoft.unittest;
 public class FizzBuzz {
     public String stringFor(int number) {
 
+        if (number < 1 || number > 100)
+            throw new IllegalArgumentException();
+
         if (number % 15 == 0) {
             return "FizzBuzz";
         } else if (number % 3 == 0) {
