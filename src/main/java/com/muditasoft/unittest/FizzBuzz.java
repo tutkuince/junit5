a0cc71a076a459c7635e25aefe57fe96fbfa6a24
tutkuince <tutku.ince@outlook.com>
2020-04-09 15:51:18 +0300

    First test case is passed

--- src/main/java/com/muditasoft/unittest/FizzBuzz.java
+++ src/main/java/com/muditasoft/unittest/FizzBuzz.java
@@ -1,5 +1,12 @@
 package com.muditasoft.unittest;
 
 public class FizzBuzz {
+    public String stringFor(int number) {
 
+        if (number % 3 == 0) {
+            return "Fizz";
+        }
+
+        return null;
+    }
 }
--- src/test/java/com/muditasoft/unittest/FizzBuzzTest.java
+++ src/test/java/com/muditasoft/unittest/FizzBuzzTest.java
@@ -1,7 +1,14 @@
 package com.muditasoft.unittest;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
 
-
+    @Test
+    void returnFizzWhenTheNumberIsDividedByThree() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("Fizz", fizzBuzz.stringFor(3));
+    }
 }
