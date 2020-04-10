8812f42a7e2a49eff5e0855e9e3853299d3ae595
tutkuince <tutku.ince@outlook.com>
2020-04-10 11:28:38 +0300

    @DisplayName kullanımı

---
+++ src/test/java/com/muditasoft/junit5/_01basics/DisplayNameCalculatorTest.java
@@ -0,0 +1,20 @@
+package com.muditasoft.junit5._01basics;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Hesap Makinesı Testler")
+public class DisplayNameCalculatorTest {
+
+    @Test
+    @DisplayName("☆tam sayıların toplanması senaryosu😎")
+    void additionOfTwoIntegers() {
+
+    }
+
+    @Test
+    @DisplayName("☆tam sayıların çıkarılması senaryosu😎")
+    void substractionOfTwoIntegers() {
+
+    }
+}
