92abf9398652542dd97641ed69e1bdcce581dff2
tutkuince <tutku.ince@outlook.com>
2020-04-10 11:51:10 +0300

    shouldCreateStudentWithIdNameAndSurnameWithGroupedAssertions

--- src/test/java/com/muditasoft/courserecord/model/StudentTest.java
+++ src/test/java/com/muditasoft/courserecord/model/StudentTest.java
@@ -52,5 +52,50 @@ public class StudentTest {
         assertNotSame(ahmet, student);
     }
 
+    @Test
+    @DisplayName("Test every student must have an id, name and surname with grouped assertions")
+    @Tag("createStudent")
+    void shouldCreateStudentWithIdNameAndSurnameWithGroupedAssertions() {
+
+        /**
+         * grouped assertions
+         * failed grouped assertions
+         * dependent assertions
+         */
+
+        // In a grouped assertion all assertions are executed,
+        Student mucahit = new Student("1", "Mucahit", "Kurt");
+
+        assertAll("Student's name check",
+                () -> assertEquals("Mucahit", mucahit.getName()),
+                () -> assertEquals("Mucahit", mucahit.getName(), "Student's name"),
+                () -> assertNotEquals("Mucahitt", mucahit.getName(), "Student's name")
+        );
+
+        // and any failures will be reported together.
+        assertAll("Student's name character check",
+                () -> assertTrue(mucahit.getName().startsWith("M")),
+                () -> assertTrue(mucahit.getName().startsWith("M"), () -> "Student's name " + "starts with Mu"),
+                () -> assertFalse(() -> {
+                    Student mehmet = new Student("id1", "Mehmet", "Can");
+                    return mehmet.getName().endsWith("M");
+                }, () -> "Student's name " + "ends with M")
+        );
+
+        //dependent assertions
+        assertAll(() -> {
+                    final Student ahmet = new Student("2", "Ahmet", "Yilmaz");
+
+                    assertArrayEquals(new String[]{"Mucahit", "Ahmet"}, Stream.of(mucahit, ahmet).map(Student::getName).toArray());
+                },
+                () -> {
+                    Student student = mucahit;
+                    final Student ahmet = new Student("2", "Ahmet", "Yilmaz");
+
+                    assertSame(mucahit, student); // mucahit == student
+                    assertNotSame(student, ahmet);
+                });
+    }
+
 
 }
