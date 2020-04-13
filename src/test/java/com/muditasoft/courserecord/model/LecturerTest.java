9d2559e017a263f3d46fbe864ddbcff38d670106
tutkuince <tutku.ince@outlook.com>
2020-04-13 20:38:10 +0300

    Test scenarios declared

---
+++ src/test/java/com/muditasoft/courserecord/model/LecturerTest.java
@@ -0,0 +1,15 @@
+package com.muditasoft.courserecord.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LecturerTest {
+
+    /**
+     * When a lecturer course record is added to lecturer then lecturer course record size increase
+     * Lecturer course record has lecturer when added to lecturer
+     * Throws illegal argument exception when a null course is added to lecturer
+     * Throws not active semester exception when a course is added for not active semester.
+     *
+     * */
+
+}
